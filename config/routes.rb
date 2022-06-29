fd72c301d87da7c5fba0a95fbb41ee4d9f4a02d1
Valentine <valentinetan91@gmail.com>
2022-06-29 07:02:33 +0000

    Updated all static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
   get 'static_pages/help'
+  get 'static_pages/about'
+  get 'static_pages/contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   
-  root 'application#hello'
+  root "static_pages#home"
 end
