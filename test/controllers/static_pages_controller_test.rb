135e2359a352094cd4c856b5bf1ee0138b42a8f4
Valentine <valentinetan91@gmail.com>
2022-06-29 09:04:15 +0000

    added styling to pages and introduce bootstrap into gemlist

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -10,7 +10,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_url
     assert_response :success
-    assert_select "title", "#{@base_title}"
+    assert_select "title", @base_title
   end
 
   test "should get help" do
