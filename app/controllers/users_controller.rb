b756a86d79285bcd426210055ee14a144ef0e9b5
Valentine <valentinetan91@gmail.com>
2022-06-30 14:31:36 +0000

    completed user sign workflow and added validation

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,46 @@
+class UsersController < ApplicationController
+  
+  def index
+  end
+  
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)    # Not the final implementation!
+    if @user.save
+      flash[:success] = "Welcome to our Sample Application"
+      redirect_to @user
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+  end
+  
+  
+  private
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
+  def user_params
+    params.require(:user).permit(:name, :first_name, :last_name, :email, :password, :password_confirmation)
+  end
+end
