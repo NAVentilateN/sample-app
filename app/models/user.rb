d0a4497d9f53003bcf8e231cd5bf55adfcf9c45d
Valentine <valentinetan91@gmail.com>
2022-06-30 04:21:35 +0000

    added user validation and validation test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+  
+  VALID_EMAIL_REGEX= /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i
+  
+  validates :first_name, :last_name, presence: true
+  validates :name, presence: true, uniqueness:{case_sensetive:false}, length: { maximum: 20 }
+  validates :email , presence: true, uniqueness:{case_sensetive:false},format:{with:VALID_EMAIL_REGEX,multiline:true}
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,69 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  def setup
+    @user = users(:first_user)
+    @user2 = users(:second_user)
+  end
+  
+  test "invalid user if name is empty" do
+    @user.name = ""
+    assert_not @user.valid?
+  end
+  
+  test "invalid user if email is empty" do
+    @user.email = ""
+    assert_not @user.valid?
+  end
+  
+  test "invalid user if first name is empty" do
+    @user.first_name = ""
+    assert_not @user.valid?
+  end
+  
+  test "invalid user if last name is empty" do
+    @user.last_name = ""
+    assert_not @user.valid?
+  end
+  
+  test "valid user if all fields are not empty and email format is correct" do
+    assert @user.valid?
+  end
+  
+  test "invalid user if all user name is more than 20 character long" do
+    @user.name = "a"*21
+    assert_not @user.valid?
+  end
+  
+  test "invalid user if all username is already taken" do
+    @user.save
+    @user2.name = 'first'
+    assert_not @user2.valid?
+  end
+  
+   test "invalid user if all user email is already taken" do
+    @user.save
+    @user2.email = 'first@first.com'
+    assert_not @user2.valid?
+  end
+  
+  test "invalid user if all user email format is incorrect" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar/baz.com foo@bar+baz.com]
+                           
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+  
+  
+  test "user email should be downcase after saving into database" do
+    email = "FiRsT@FiRsT.com"
+    @user.email = email
+    @user.save
+    assert_not_equal @user.email, email
+    assert_equal @user.email, email.downcase
+  end
 end
