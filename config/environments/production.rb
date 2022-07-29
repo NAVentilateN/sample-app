30696609030c87822740c7f2ba7ac291cfdcd87f
Valentine <valentinetan91@gmail.com>
2022-07-29 09:19:15 +0000

    updated prod file for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,13 +68,13 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'aqueous-dawn-17502.herokuapp.com'
+  host = 'https://aqueous-dawn-17502.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
+    :user_name      => 'apikey',
     :password       => ENV['SENDGRID_API_KEY'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
