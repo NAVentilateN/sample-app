3e32bef77843caca14dd0425e8fc017c811022f3
Valentine <valentinetan91@gmail.com>
2022-07-29 09:11:33 +0000

    typo in production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,7 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => 'ENV['SENDGRID_USERNAME'],
+    :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_API_KEY'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
